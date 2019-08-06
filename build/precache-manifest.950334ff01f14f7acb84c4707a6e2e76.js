4ced610e3fb8231b2e32ea56e1660b9f7bbc92f1
Thomas Girgenti <thomasg@disroot.org>
2019-08-06 15:35:44 -0600

    commit build folder

---
+++ build/precache-manifest.950334ff01f14f7acb84c4707a6e2e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17965132f317d9cb188b20d7412ff3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6957d8a48c95b583f3",
+    "url": "/static/css/main.731f8a1b.chunk.css"
+  },
+  {
+    "revision": "03897efa9f2efe26fc8a",
+    "url": "/static/js/2.9875a667.chunk.js"
+  },
+  {
+    "revision": "7c6957d8a48c95b583f3",
+    "url": "/static/js/main.964d50e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
