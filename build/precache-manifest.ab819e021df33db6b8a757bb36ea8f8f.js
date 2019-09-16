5dca8d70cf44d0bdc45f926861c8a43d92a209d7
Thomas Girgenti <thomasg@disroot.org>
2019-09-16 13:23:32 -0600

    Build w/ new icons and version

---
+++ build/precache-manifest.ab819e021df33db6b8a757bb36ea8f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6029345b829ff4e85abbff1eefc3602e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5091fd5842e82a8c4ad",
+    "url": "/static/css/main.16416a9f.chunk.css"
+  },
+  {
+    "revision": "fe795ef5fa2e4a32f084",
+    "url": "/static/js/2.967999e0.chunk.js"
+  },
+  {
+    "revision": "d5091fd5842e82a8c4ad",
+    "url": "/static/js/main.0e91974b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
