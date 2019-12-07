b47a63527862f4c276a76172823e8dd79462b491
Thomas Girgenti <thomasg@disroot.org>
2019-12-06 17:39:26 -0700

    update logged-out error handling and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Login from './components/Login';
 import AddBookmark from './components/AddBookmark';
 
 function App() {
-    const [token, setToken] = useState(localStorage.getItem('JWT'));
+	const [token, setToken] = useState(localStorage.getItem('JWT'));
 
-    return token === null
-           ? <Login update={() => setToken(localStorage.getItem('JWT'))}/>
-           : <AddBookmark update={() => setToken(localStorage.getItem('JWT'))}/>
+	return token === null
+		? <Login update={() => setToken(localStorage.getItem('JWT'))}/>
+		: <AddBookmark update={() => setToken(localStorage.getItem('JWT'))}/>;
 
 }
 
